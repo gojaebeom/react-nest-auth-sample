a27a977c104e414f779034f2e38cbe6a02419cec
gojaebeom <const.gjb@gmail.com>
2022-03-18 11:56:41 +0900

    회원가입 API part 1 > client로부터 값을 받고 저장하기 이전 이메일 중첩 채크 로직 완료

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import UserService from './user.service';
 
 @Controller('users')
@@ -7,5 +7,11 @@ export default class UserController {
   @Get('/me')
   getCurrentUser() {
     return this.userService.getCurrentUser();
+  }
+
+  @Post()
+  signUp(@Body() body) {
+
+    return this.userService.signUp(body);
   }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,8 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, HttpException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import { Repository } from 'typeorm';
+import User from './entities/user.entity';
 
 @Injectable()
 export default class UserService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
   getCurrentUser() {
     return 'get my profile data';
+  }
+
+  signUp(body: any) {
+    console.debug(body);
+    const findUser = this.userRepository.findOne({
+      where: { email: body.email },
+    });
+    console.debug(findUser);
+    if (findUser) {
+      throw new ConflictException('이미 존재하는 이메일 입니다. 😩');
+    }
   }
 }
