7058e191d8644f10f536b780e2e3df2ba7d06031
gojaebeom <const.gjb@gmail.com>
2022-03-18 11:22:35 +0900

    요청에대한 API 속도 측정 인터셉터 구현및 추가

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { TimmerInterceptor } from './common/interceptors/timmer.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TimmerInterceptor());
 
   const PORT = process.env.APP_PORT;
   await app.listen(PORT);
