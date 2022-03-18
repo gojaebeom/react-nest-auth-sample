7fc08ec6764a8da77ae7649ba808ed1c71b91100
gojaebeom <const.gjb@gmail.com>
2022-03-18 10:34:13 +0900

    src 디렉토리의 app.* 파일들 app 디렉토리로 묶어서 배치

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
   const PORT = process.env.APP_PORT;
   await app.listen(PORT);
 }
